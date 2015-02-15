7232befec049271dcd92ca311b4eff98abc3b093
Hieu T. Le <hieuletrung@gmail.com>
2015-02-15 14:32:43 +0700

    Update main for AlljoynApp

--- linux/src/AlljoynApp.cpp
+++ linux/src/AlljoynApp.cpp
@@ -1,4 +1,13 @@
-int main()
+#include "common.h"
+#include "AlljoynApp.h"
+
+using namespace ajn;
+using namespace services;
+
+int main(int argc, char**argv, char**envArg)
 {
+    std::cout << "AllJoyn Library version: " << ajn::GetVersion() << std::endl;
+    std::cout << "AllJoyn Library build info: " << ajn::GetBuildInfo() << std::endl;
+
     return 0;
 }
