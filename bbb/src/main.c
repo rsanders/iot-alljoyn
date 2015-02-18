c98662f2e6584366496bcb757d4b1b97a074bbe5
Hieu T. Le <hieuletrung@gmail.com>
2015-02-19 00:51:44 +0700

    Update build and README for BBB.

--- bbb/src/main.c
+++ bbb/src/main.c
@@ -1,4 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <aj_debug.h>
+#include <alljoyn.h>
+
+int AJ_Main(void)
+{
+    AJ_Status status = AJ_OK;
+    AJ_BusAttachment bus;
+    uint8_t connected = FALSE;
+    uint8_t done = FALSE;
+    uint32_t sessionId = 0;
+
+    /*
+     * One time initialization before calling any other AllJoyn APIs
+     */
+    AJ_Initialize();
+
+    return status;
+}
+
+#ifdef AJ_MAIN
 int main()
 {
-    return 0;
+    return AJ_Main();
 }
+#endif
